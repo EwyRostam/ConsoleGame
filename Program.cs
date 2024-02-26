e8f12b375e74f0078daef349c6266350c0cc501e
Ewy Rostam <ewy.rost@gmail.com>
2024-02-26 13:54:28 +0100

    One star falls flawlessly

--- Program.cs
+++ Program.cs
@@ -7,13 +7,23 @@ var randomizer = new Random();
 while (!Console.KeyAvailable)
 {
     Console.CursorVisible = false;
-    position++;
-         
+
+    if (position < Console.BufferHeight - 2)
+    {
+        position += 1;
+    }
+    else
+    {
+        position = 1;
+        Console.SetCursorPosition(0, Console.BufferHeight - 2);
+        Console.Write("\r" + new string(' ', Console.WindowWidth) + "\r");
+    }
+
     Thread.Sleep(500);
     Console.SetCursorPosition(windowWidth / 2, position);
     Console.WriteLine('*');
-    Console.WriteLine();
-    Console.WriteLine();
-    Console.SetCursorPosition(0, position -1);
+    Console.SetCursorPosition(0, position - 1);
     Console.Write("\r" + new string(' ', Console.WindowWidth) + "\r");
 }
+
+
