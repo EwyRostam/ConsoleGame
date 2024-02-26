19359c728da9c555186f62b907986b17da432c85
Ewy Rostam <ewy.rost@gmail.com>
2024-02-26 13:28:37 +0100

    Make one star fall

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿int windowWidth = Console.WindowWidth;
 int windowHeight = Console.WindowHeight;
-int position = 0;
+int position = 1;
 
 var randomizer = new Random();
 
@@ -8,12 +8,12 @@ while (!Console.KeyAvailable)
 {
     Console.CursorVisible = false;
     position++;
+         
     Thread.Sleep(500);
-
-    Console.SetCursorPosition(windowWidth / 2, position -1);
-    Console.Write("\r" + new string(' ', Console.WindowWidth) + "\r");
     Console.SetCursorPosition(windowWidth / 2, position);
     Console.WriteLine('*');
     Console.WriteLine();
     Console.WriteLine();
+    Console.SetCursorPosition(0, position -1);
+    Console.Write("\r" + new string(' ', Console.WindowWidth) + "\r");
 }
