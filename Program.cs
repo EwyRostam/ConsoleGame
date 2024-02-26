24f9cc7704b28873bbbfc521aa533e252a67f906
Ewy Rostam <ewy.rost@gmail.com>
2024-02-26 13:00:28 +0100

    Try to make stars fall

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int windowWidth = Console.WindowWidth;
+int windowHeight = Console.WindowHeight;
+int position = 0;
+
+var randomizer = new Random();
+
+while (!Console.KeyAvailable)
+{
+    position++;
+    Thread.Sleep(500);
+    Console.CursorVisible = false;
+    Console.SetCursorPosition(randomizer.Next(windowWidth), position);
+    Console.WriteLine('*');
+    Console.WriteLine();
+    Console.WriteLine();
+
+}
